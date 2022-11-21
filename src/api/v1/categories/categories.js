3ecbbec98e9a35bd834de138c7182c84f20a63c4
Zohaib <zohaib5424@gmai.com>
2022-11-21 10:49:49 +0500

    Next test

--- src/api/v1/categories/categories.js
+++ src/api/v1/categories/categories.js
@@ -1,13 +1,14 @@
-import mongoose from 'mongoose';
-import { v1 as uuidv1 } from 'uuid';
+import mongoose from "mongoose";
+import { v1 as uuidv1 } from "uuid";
 
 /**
  * Categories Schema
  */
 
 //testing
+//next test
 const CategoriesSchema = new mongoose.Schema({
-  cId: { type: String, default: _ => uuidv1() },
+  cId: { type: String, default: (_) => uuidv1() },
   name: { type: String },
   createdAt: { type: Date, default: Date.now },
 });
@@ -15,4 +16,4 @@ const CategoriesSchema = new mongoose.Schema({
 /**
  * @typedef Categories
  */
-export default mongoose.model('Categories', CategoriesSchema);
+export default mongoose.model("Categories", CategoriesSchema);
